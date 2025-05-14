2e65170eea39ffbd3bf89387719b495423733eb3
Axel <axel.j.zuniga@gmail.com>
2025-05-14 14:48:10 -0600

    cleaned files

--- src/components/ui/Administrativa/ChatInterno/ChatWindow.tsx
+++ src/components/ui/Administrativa/ChatInterno/ChatWindow.tsx
@@ -6,7 +6,7 @@ const ChatWindow = () => {
   return (
     <div className="h-full flex flex-col border rounded shadow bg-white">
       <div className="flex-1 overflow-y-auto p-4">
-        {/* Aquí irían los mensajes */}
+        {  }
         <MessageBubble message="Hola, ¿hay alguna novedad?" from="user" />
         <MessageBubble message="No por ahora, todo tranquilo." from="admin" />
       </div>
